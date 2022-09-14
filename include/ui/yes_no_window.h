37844ceb390761e439d9c8c3bb0a2708994013e0
benjamimFG <ben.freitasg@gmail.com>
2022-09-14 19:43:55 -0300

    refactor: Refactor yes_no_window.c and its' header file
    * Add documentation to functions in header file
    
    * Add const keyword to parameter question of draw_yes_no_window
    
    * Change return type from get_answer function from int to bool
    
    * Remove useless switch case from get_answer function implementation

--- include/ui/yes_no_window.h
+++ include/ui/yes_no_window.h
@@ -4,7 +4,21 @@
 #include <ncurses.h>
 #include <stdbool.h>
 
-void draw_yes_no_window(WINDOW* window, char* question);
-int get_answer(WINDOW* window);
+/**
+ * Draws a question on the window and appends the string "[y/N]" to it.
+ * 
+ * @param window Pointer to the ncurses WINDOW to be drawn
+ * @param question Pointer to the string with the question to be printed to the window
+*/
+void draw_yes_no_window(WINDOW* window, const char* question);
+
+/**
+ * Gets a character from window and returns true or false depending on the user answer.
+ * 
+ * @param window Pointer to the ncurses WINDOW to get the character from
+ * 
+ * @returns true if the user presses 'y' or 'Y', false otherwise
+*/
+bool get_answer(WINDOW* window);
 
 #endif
--- src/ui/yes_no_window.c
+++ src/ui/yes_no_window.c
@@ -1,7 +1,7 @@
 #include <ncurses.h>
 #include <stdbool.h>
 
-void draw_yes_no_window(WINDOW* window, char* question) {
+void draw_yes_no_window(WINDOW* window, const char* question) {
   box(window, 0, 0);
 
   mvwprintw(window, 1, 1, "%s [y/N]", question);
@@ -9,21 +9,11 @@ void draw_yes_no_window(WINDOW* window, char* question) {
   wrefresh(window);
 }
 
-int get_answer(WINDOW* window) {
+bool get_answer(WINDOW* window) {
   noecho();
   curs_set(0);
 
   char c = wgetch(window);
 
-  switch (c) {
-  case 'y':
-  case 'Y':
-    return true;
-    break;
-  
-  default:
-    break;
-  }
-
-  return false;
+  return c == 'y' || c == 'Y';
 }
