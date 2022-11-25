8d8e7d7eccacc60bf54f837516e30fb7fc9d5fc2
benjamimFG <ben.freitasg@gmail.com>
2022-11-24 22:37:56 -0300

    add function to check if a string starts with a character

--- include/utils/string_utils.h
+++ include/utils/string_utils.h
@@ -1,8 +1,9 @@
 #ifndef STRING_UTILS_H
 #define STRING_UTILS_H
 
-#include <string.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * Takes a character 'c' and repeats it 'n' times into a string.
@@ -13,5 +14,15 @@
  * @return Returns a pointer to a string of length 'n'
  */
 char* char_repeat(const char c, const int n);
+
+/**
+ * Checks if a string starts with a character.
+ * 
+ * @param str Pointer to the string to be checked
+ * @param c Character to check if it's the first on str
+ * 
+ * @returns Returns true if str starts with c, false otherwise
+*/
+bool starts_with(const char* str, const char c);
 
 #endif
--- src/utils/string_utils.c
+++ src/utils/string_utils.c
@@ -1,15 +1,22 @@
-#include <string.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "string_utils.h"
 
 char* char_repeat(const char c, const int n) {
   char* str = malloc(n * sizeof(char));
-  memset(str, 0, n);
+  memset(str, 0, n * sizeof(char));
 
   for (int i = 0; i < n; ++i) {
     str[i] = c;
   }
 
   return str;
+}
+
+bool starts_with(const char* str, const char c) {
+  if (strlen(str) == 0) return false;
+
+  return str[0] == c;
 }
